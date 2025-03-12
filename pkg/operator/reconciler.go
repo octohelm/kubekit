866464095748b148c697c652bc07681a3d0cc8ef
Morlay <morlay.null@gmail.com>
2025-03-12 17:58:48 +0800

    chore(dep): updates

--- pkg/operator/reconciler.go
+++ pkg/operator/reconciler.go
@@ -10,10 +10,16 @@ type ReconcilerRegistry interface {
 	RegisterReconciler(reconcilers ...Reconciler) error
 }
 
+type SchemeAdder interface {
+	AddToScheme(s *runtime.Scheme) error
+}
+
 type Reconciler interface {
 	SetupWithManager(mgr controllerruntime.Manager) error
 }
 
-type SchemeAdder interface {
-	AddToScheme(s *runtime.Scheme) error
+type ReconcilerFunc func(mgr controllerruntime.Manager) error
+
+func (fn ReconcilerFunc) SetupWithManager(mgr controllerruntime.Manager) error {
+	return fn(mgr)
 }
